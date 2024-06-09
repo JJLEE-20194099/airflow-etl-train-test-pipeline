b9bc268f9686729a6151c38c205df084fbb8ad32
JJLEE-20194099 <long.lt194099@gmail.com>
2024-06-09 16:24:42 +0000

    crate Ensemble model

--- src/helpers/mlflow_tool.py
+++ src/helpers/mlflow_tool.py
@@ -20,3 +20,26 @@ def stack_with_onehot(scores):
         new_scores_l.append(n_score)
     scores = np.array(new_scores_l)
     return scores
+
+def mean_max(scores):
+    scores = np.mean(scores, 0)
+    scores = np.argmax(scores, -1)
+    return scores
+
+class Ensemble(mlflow.pyfunc.PythonModel):
+    def __init__(self, models_list, ensemble_method=mean_max, stack_scores=stack_with_onehot,
+                 models_all_cached=False, force_predict_function=False):
+        super().__init__()
+        if models_all_cached:
+            self.models_list = []
+            for model in models_list:
+                self.models_list.append(load_model_or_uri(model))
+        else:
+            self.models_list = models_list
+
+        self.scores_list = None
+        self.meta_model = None
+        self._ensemble_method = ensemble_method
+        self._stack_scores = stack_scores
+        self._force_predict_function = force_predict_function
+
