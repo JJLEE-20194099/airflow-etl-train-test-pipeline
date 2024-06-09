679dd17e84bd6b9ab92ebcdb0fdcf3515f530f22
JJLEE-20194099 <long.lt194099@gmail.com>
2024-06-09 16:27:51 +0000

    update fit & predict in metamodel

--- src/helpers/mlflow_tool.py
+++ src/helpers/mlflow_tool.py
@@ -43,3 +43,47 @@ class Ensemble(mlflow.pyfunc.PythonModel):
         self._stack_scores = stack_scores
         self._force_predict_function = force_predict_function
 
+    def predict_scores(self, data, cache_scores=False):
+        scores_list = []
+        for model in self.models_list:
+            loaded_model = load_model_or_uri(model)
+            if not self._force_predict_function:
+                if hasattr(loaded_model, "_model_impl") and hasattr(loaded_model._model_impl, "predict_proba"):
+                    scores = loaded_model._model_impl.predict_proba(data)
+                elif hasattr(loaded_model, "predict_proba"):
+                    scores = loaded_model.predict_proba(data)
+                else:
+                    scores = loaded_model.predict(data)
+            else:
+                scores = loaded_model.predict(data)
+
+            scores_list.append(scores)
+
+        if cache_scores:
+            self.scores_list = scores_list
+        return scores_list
+
+    def aggregate_scores(self, scores):
+        if self._ensemble_method == "meta_model":
+            return self.meta_model.predict(scores)
+        else:
+            return self._ensemble_method(scores)
+
+    def fit(self, model, data, target, force_scores_compute=True, cache_scores=True):
+        if force_scores_compute or self.scores_list is None:
+            data = self.predict_scores(data, cache_scores)
+        else:
+            data = self.scores_list
+
+        data = self._stack_scores(data)
+        model.fit(data, target)
+        self.meta_model = model
+
+    def set_meta_model(self, model):
+        self.meta_model = copy.deepcopy(model)
+
+    def predict(self, context, data):
+        scores = self.predict_scores(data, cache_scores=False)
+        scores = self._stack_scores(scores)
+        return self.aggregate_scores(scores)
+
