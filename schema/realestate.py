fac510164f5cd5e9ad92cfb8c3379e1cfa3b9b5f
JJLEE-20194099 <long.lt194099@gmail.com>
2024-06-21 04:05:33 +0000

    preprocess body

--- schema/realestate.py
+++ schema/realestate.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel, model_validator
 from typing import Union, List, Optional
 import json
 
+from schema.preprocess.text import preprocess_text
+
 class GeolocationModel(BaseModel):
     latitude: float
     longitude: float
@@ -49,6 +51,32 @@ class RealEstateData(BaseModel):
             assert  ward in data[city]["ward"], f"Valid wards: {data[city]['ward']}"
 
             assert  street in data[city]["street"], f"Valid streets: {data[city]['street']}"
+
+            for key in ['ward', 'district', 'street', 'prefixDistrict']:
+                field_values[key] = preprocess_text(field_values[key])
+
+            if district in ['mê linh',
+                'ba vì',
+                'phúc thọ',
+                'thạch thất',
+                'mỹ đức',
+                'sơn tây',
+                'quốc oai',
+                'quốc oai']:
+
+                field_values['district'] = 'suburb_west'
+
+            if district in ['sóc sơn', 'đan phượng']:
+                field_values['district'] = 'suburb_north'
+
+            if district in ['thanh oai',
+                'ứng hòa',
+                'phú xuyên',
+                'thường tín',
+                'chương mỹ'
+            ]:
+                field_values['district'] = 'suburb_south'
+
         return field_values
 
 
