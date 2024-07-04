c0db04df387f193832168b4ae98266548cba64ab
JJLEE-20194099 <long.lt194099@gmail.com>
2024-07-04 06:55:00 +0000

    update general dag

---
+++ single_model_training_dag_generation.py
@@ -0,0 +1,49 @@
+import warnings
+
+from airflow import DAG
+from airflow.decorators import task, dag
+from utils.train_func import train_model_by_city_data_and_feature_version
+from datetime import datetime, timedelta
+
+from dotenv import load_dotenv
+load_dotenv(override=True)
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+def create_dag(dag_id, schedule, tags, default_args, city, model_name):
+    @dag(dag_id=dag_id, schedule=schedule, tags = tags, default_args=default_args, catchup=False)
+    def taskflow():
+
+        @task(task_id=f"train_{city}_{model_name}_model_v0", retries=0)
+        def train_v0():
+            train_model_by_city_data_and_feature_version(city = city, version = 0, model_name = model_name)
+
+        @task(task_id=f"train_{city}_{model_name}_model_v1", retries=0)
+        def train_v1():
+            train_model_by_city_data_and_feature_version(city = city, version = 1, model_name = model_name)
+
+        @task(task_id=f"train_{city}_{model_name}_model_v2", retries=0)
+        def train_v2():
+            train_model_by_city_data_and_feature_version(city = city, version = 2, model_name = model_name)
+
+        @task(task_id=f"train_{city}_{model_name}_model_v4", retries=0)
+        def train_v4():
+            train_model_by_city_data_and_feature_version(city = city, version = 4, model_name = model_name)
+
+        @task(task_id=f"train_{city}_{model_name}_model_v5", retries=0)
+        def train_v5():
+            train_model_by_city_data_and_feature_version(city = city, version = 5, model_name = model_name)
+
+        [train_v0(), train_v1(), train_v2(), train_v4(), train_v5()]
+
+    generated_dag = taskflow()
+
+for city in ['hcm', 'hn']:
+    for model_name in ['abr', 'cat', 'etr', 'gbr', 'knr', 'la', 'lgbm', 'linear', 'mlp', 'rf', 'ridge', 'xgb']:
+        dag_id = f"train_{city}_{model_name}_model"
+        default_args = {"owner": "airflow", "start_date": datetime(2024, 7, 1)}
+        schedule = "@daily"
+
+        globals()[dag_id] = create_dag(dag_id, schedule, [dag_id], default_args, city, model_name)
+
--- train_hcm_rf_model.py
+++ train_hcm_rf_model.py
@@ -1,38 +1,38 @@
-import warnings
+# import warnings
 
-from airflow import DAG
-from airflow.decorators import task, dag
-from utils.train_func import train_model_by_city_data_and_feature_version
-from datetime import datetime, timedelta
+# from airflow import DAG
+# from airflow.decorators import task, dag
+# from utils.train_func import train_model_by_city_data_and_feature_version
+# from datetime import datetime, timedelta
 
-from dotenv import load_dotenv
-load_dotenv(override=True)
-import os
-import warnings
-warnings.filterwarnings('ignore')
+# from dotenv import load_dotenv
+# load_dotenv(override=True)
+# import os
+# import warnings
+# warnings.filterwarnings('ignore')
 
-@dag("train_hcm_rf_model", tags = ["train_hcm_rf_model"], schedule='0 10,19 * * *', catchup=False, start_date=datetime(2024, 6, 6))
-def taskflow():
+# @dag("train_hcm_rf_model", tags = ["train_hcm_rf_model"], schedule='0 10,19 * * *', catchup=False, start_date=datetime(2024, 6, 6))
+# def taskflow():
 
-    @task(task_id="train_hcm_rf_model_v0", retries=0)
-    def train_v0():
-        train_model_by_city_data_and_feature_version(city = 'hcm', version = 0, model_name = 'rf')
+#     @task(task_id="train_hcm_rf_model_v0", retries=0)
+#     def train_v0():
+#         train_model_by_city_data_and_feature_version(city = 'hcm', version = 0, model_name = 'rf')
 
-    @task(task_id="train_hcm_rf_model_v1", retries=0)
-    def train_v1():
-        train_model_by_city_data_and_feature_version(city = 'hcm', version = 1, model_name = 'rf')
+#     @task(task_id="train_hcm_rf_model_v1", retries=0)
+#     def train_v1():
+#         train_model_by_city_data_and_feature_version(city = 'hcm', version = 1, model_name = 'rf')
 
-    @task(task_id="train_hcm_rf_model_v2", retries=0)
-    def train_v2():
-        train_model_by_city_data_and_feature_version(city = 'hcm', version = 2, model_name = 'rf')
+#     @task(task_id="train_hcm_rf_model_v2", retries=0)
+#     def train_v2():
+#         train_model_by_city_data_and_feature_version(city = 'hcm', version = 2, model_name = 'rf')
 
-    @task(task_id="train_hcm_rf_model_v4", retries=0)
-    def train_v4():
-        train_model_by_city_data_and_feature_version(city = 'hcm', version = 4, model_name = 'rf')
+#     @task(task_id="train_hcm_rf_model_v4", retries=0)
+#     def train_v4():
+#         train_model_by_city_data_and_feature_version(city = 'hcm', version = 4, model_name = 'rf')
 
-    @task(task_id="train_hcm_rf_model_v5", retries=0)
-    def train_v5():
-        train_model_by_city_data_and_feature_version(city = 'hcm', version = 5, model_name = 'rf')
+#     @task(task_id="train_hcm_rf_model_v5", retries=0)
+#     def train_v5():
+#         train_model_by_city_data_and_feature_version(city = 'hcm', version = 5, model_name = 'rf')
 
-    [train_v0(), train_v1(), train_v2(), train_v4(), train_v5()]
-dag = taskflow()
+#     [train_v0(), train_v1(), train_v2(), train_v4(), train_v5()]
+# dag = taskflow()
